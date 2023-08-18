c833ec21fab4ef2434a8f0a38573c8309a8eb8da
Gurbaaz Singh Gill <gurbaaz19@gmail.com>
2023-08-18 17:59:24 +0530

    Level 2

--- Assets/Scripts/ItemCollector.cs
+++ Assets/Scripts/ItemCollector.cs
@@ -1,14 +1,18 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading;
 using TMPro;
 using UnityEngine;
+
 public class ItemCollector : MonoBehaviour
 {
+    private int isBoxCollectedTime= 0;
     private int cherriesCollected;
     [SerializeField] private AudioSource collectSound;
-    
+
     [SerializeField] private TextMeshProUGUI cherriesCollectedText;
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("Cherry"))
@@ -17,6 +21,29 @@ public class ItemCollector : MonoBehaviour
             Destroy(collision.gameObject);
             cherriesCollected++;
             cherriesCollectedText.text = $"Cherries Collected: {cherriesCollected.ToString()}";
+        }
+
+        if (collision.gameObject.CompareTag("Rock Box"))
+        {
+            collectSound.Play();
+            Destroy(collision.gameObject);
+            cherriesCollectedText.text =
+                $"Cherries Collected: {cherriesCollected.ToString()}\nYou Collected a Message Box, you are doing great! Here are 20 cherry points for you.";
+            cherriesCollected += 20;
+            isBoxCollectedTime = 500;
+        }
+    }
+
+    private void Update()
+    {
+        if (isBoxCollectedTime!=0)
+        {
+            isBoxCollectedTime--;
+            Debug.Log(isBoxCollectedTime);
+            if (isBoxCollectedTime == 0)
+            {
+                cherriesCollectedText.text = $"Cherries Collected: {cherriesCollected.ToString()}";
+            }
         }
     }
 }
